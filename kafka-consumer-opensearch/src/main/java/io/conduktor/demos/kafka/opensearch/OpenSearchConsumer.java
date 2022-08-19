4be823263c03ea479e86181a3581fa168b519b99
Christian Rua <christian.rua@autodesk.com>
2022-08-19 09:57:52 -0500

    code to connect and leave data in the opensearch cluster, the data comes from kafka

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -6,17 +6,29 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.lucene.index.IndexReader;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.io.IOException;
 import java.lang.management.OperatingSystemMXBean;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class OpenSearchConsumer {
 
@@ -56,6 +68,24 @@ public class OpenSearchConsumer {
 
     }
 
+    private static KafkaConsumer<String, String> createKafkaConsumer(){
+
+        String boostrapServer = "127.0.0.1:9092";
+        String groupId = "consumer-opensearch-demo";
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        // create consumer
+
+        return new KafkaConsumer<>(properties);
+    }
+
     public static void main(String[] args) throws IOException {
 
         String topicName = "wikimedia";
@@ -65,9 +95,12 @@ public class OpenSearchConsumer {
         // first create an OpenSearch client
         RestHighLevelClient openSearchClient = createOpenSearchClient();
 
+        // create our Kafka Client
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
+
         // we need to create the index on OpenSearch if it doesn't exist already
 
-        try(openSearchClient) {
+        try(openSearchClient; consumer) {
 
             boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest(topicName), RequestOptions.DEFAULT);
 
@@ -79,12 +112,35 @@ public class OpenSearchConsumer {
                 log.info("The Wikimedia Index already exists");
             }
 
+            consumer.subscribe(Collections.singleton("wikimedia.recentchange"));
 
+            while (true){
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
+                int recordCount = records.count();
+                log.info("Received " + recordCount + " records(s)");
+
+                for (ConsumerRecord<String, String> record : records){
+
+                    try {
+                        // Send the record into OpenSearch
+                        IndexRequest indexRequest = new IndexRequest(topicName)
+                                .source(record.value(), XContentType.JSON);
+
+                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+                        log.info(response.getId());
+                    } catch (Exception e) {
+                        log.info("We have a problem here " + e);
+                    }
+
+
+                }
+
+            }
 
         }
 
 
-        // create our Kafka Client
+
 
         // main code logic
 
